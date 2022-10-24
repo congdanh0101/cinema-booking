0d9d7e5a79d5675f0a7e901422360e2ed952dd76
minhducnt <minhduc.nt1602@gmail.com>
2022-10-24 22:15:36 +0700

    Add Layouts & Components for Admin
    - Thêm các layout và component của Admin
     > Sidebar
     > Header
     > Footer

--- src/Utils/Themes/index.js
+++ src/Utils/Themes/index.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { createMuiTheme } from '@material-ui/core/styles';
+import '@material-ui/core/styles';
 // import palette from './palette';
 import paletteDark from './paletteDark';
 import typography from './typography';
