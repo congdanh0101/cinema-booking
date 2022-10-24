1ba7d1958af0d553a7f87ecf3d1dead5cd0b417a
minhducnt <minhduc.nt1602@gmail.com>
2022-10-24 19:53:17 +0700

    Rebuild UI from scatch
    - Đập và xây lại UI hoàn toàn mới
    - Xây dựng UI admin

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-// @ts-nocheck
-import React, { Component } from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
+import logo from './logo.svg';
+import './App.css';
 
-//Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './store/actions';
-
-import theme from './theme';
-import { Alert } from './components';
-import { pageCursors } from './utils';
-import Routes from './Routes';
-
-import './assets/scss/index.scss';
-import 'typeface-montserrat';
-import { CssBaseline } from '@material-ui/core';
-
-class App extends Component {
-  componentDidMount() {
-    store.dispatch(loadUser());
-    pageCursors();
-  }
-  render() {
-    return (
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Alert />
-          <Routes />
-          <div className="cursor" id="cursor" />
-          <div className="cursor2" id="cursor2" />
-          <div className="cursor3" id="cursor3" />
-        </ThemeProvider>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
