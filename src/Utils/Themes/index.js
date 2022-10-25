ed86c7bcc1e7593198eb25c81d6f404841c08323
minhducnt <minhduc.nt1602@gmail.com>
2022-10-25 22:12:21 +0700

    Fix the wrong routes after reroutes

--- src/Utils/Themes/index.js
+++ src/Utils/Themes/index.js
@@ -1,20 +1,20 @@
 // @ts-nocheck
-import '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 // import palette from './palette';
-import paletteDark from './paletteDark';
-import typography from './typography';
+import paletteDark from './PaletteDark';
+import typography from './Typography';
 
 // read more at https://material-ui.com/customization/themes
 const theme = createMuiTheme({
-  palette: paletteDark,
-  typography,
-  zIndex: {
-    appBar: 1200,
-    drawer: 1100
-  },
-  topBar: {
-    height: '56px'
-  }
+	palette: paletteDark,
+	typography,
+	zIndex: {
+		appBar: 1200,
+		drawer: 1100,
+	},
+	topBar: {
+		height: '56px',
+	},
 });
 
 export default theme;
